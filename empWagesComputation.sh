b89b38947a23a02d4e402519f32b228373463e23
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 13:43:26 +0530

    Added: empWageComputation.sh Calculating Wage for month

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -6,7 +6,11 @@ PART_TIME_HOUR=8;
 countPresent=0;
 countAb=0
 presentHour=0;
-
+PART_TIME_SAL=0
+WORKINGDAY_IN_MONTH=20;
+day=0;
+for (( counter=1; counter <= $WORKINGDAY_IN_MONTH; counter++))
+do
 	att=$((RANDOM%3))
 	case $att in
 	"1")
@@ -15,12 +19,17 @@ presentHour=0;
 	"2")
 		echo "Part Time"
 		presentHour=$((presentHour + 8))
-		totalSal=$((totalSal + $presentHour * $WAGE_PER_HOUR))
 		;;
 		
 	*)
 		echo "Present"
-		totalSal=$((totalSal + $FULL_DAY_HOUR * $WAGE_PER_HOUR))
+		day=$((day+1))
+		;;
+
 	esac
-	echo "$totalSal"
+done
+	PART_TIME_SAL=$(($presentHour * $WAGE_PER_HOUR))
+	totalSal=$(($day * $FULL_DAY_HOUR * $WAGE_PER_HOUR))
+	echo "Salary of part time is : $PART_TIME_SAL"
+	echo "Monthly wages :  $totalSal"
 
