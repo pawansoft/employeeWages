7b34a58eb3d1beac8686c76da338a191ede4054b
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 12:54:58 +0530

    Added: empWagesComputation.sh for adding partTime Employee

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -2,14 +2,21 @@
 FULL_DAY_HOUR=8;
 WAGE_PER_HOUR=20;
 totalSal=0
+PART_TIME_HOUR=8;
 countPresent=0;
 countAb=0
+presentHour=0;
 
-	att=$((RANDOM%2))
+	att=$((RANDOM%3))
 	echo $att
 	if [ $att -eq 0 ]
 	then
 		echo "Absent"
+	elif [ $att -eq 2 ]
+	then
+		echo "Part Time"
+		presentHour=$((presentHour + 8))
+		totalSal=$((totalSal + $presentHour * $WAGE_PER_HOUR))
 		
 	else
 		echo "Present"
