f58f9a544847d450046b76ef2b7300150970b1dd
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 14:07:55 +0530

    Added: empWagesComputation.sh with limitation

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -3,13 +3,13 @@ FULL_DAY_HOUR=8;
 WAGE_PER_HOUR=20;
 totalSal=0
 PART_TIME_HOUR=8;
-countPresent=0;
+TOTAL_WORKING_HOUR=100
 countAb=0
 presentHour=0;
 PART_TIME_SAL=0
 WORKINGDAY_IN_MONTH=20;
 day=0;
-for (( counter=1; counter <= $WORKINGDAY_IN_MONTH; counter++))
+while [[ $day -le $WORKINGDAY_IN_MONTH && $presentHour -lt $TOTAL_WORKING_HOUR ]] 
 do
 	att=$((RANDOM%3))
 	case $att in
@@ -30,6 +30,6 @@ do
 done
 	PART_TIME_SAL=$(($presentHour * $WAGE_PER_HOUR))
 	totalSal=$(($day * $FULL_DAY_HOUR * $WAGE_PER_HOUR))
-	echo "Salary of part time is : $PART_TIME_SAL"
-	echo "Monthly wages :  $totalSal"
+	echo "Salary of part time is : $PART_TIME_SAL : Hours = $presentHour"
+	echo "Monthly wages :  $totalSal : day = $day"
 
