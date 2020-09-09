e3840e9cbfd4e91eba610d73529ae374456e7cc4
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 11:37:01 +0530

    Added : empWagesComputation.sh check employee Attendance Added

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Welcome to Employee Wage Computation program on master branch"
+countPresent=0;
+countAb=0
+read -p "Enter the days to check the attendance" days;
+for (( i=0; i <= $days; i++ ))
+do
+	att=$((RANDOM%2))
+	if [ $att -eq 0 ]
+	then
+		countAb=$((countAb + 1))
+	else
+		countPresent=$((countPresent + 1))
+	fi
+done
+echo "Present Days : $countPresent"
+echo "Absent Days : $countAb" 
